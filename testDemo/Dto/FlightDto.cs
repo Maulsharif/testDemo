d2798b610221c9437e58b87d1577a7d89a1fcedd
maulsharipova <maulsharipova@petrel.ai>
2022-08-01 23:26:31 +0600

    moved  configs into separate class

--- testDemo/Dto/FlightDto.cs
+++ testDemo/Dto/FlightDto.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using testDemo.Models;
 
 namespace testDemo.Dto
 {
     public class FlightDto
     {
+        [MaxLength(3)]
         public string Origin { get; set; }
+        [MaxLength(3)]
         public string Destination { get; set; }
         public DateTimeOffset Departure { get; set; }
         public DateTimeOffset Arrival { get; set; }
